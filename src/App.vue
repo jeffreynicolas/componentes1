76098b5869f9567d3b2933be5d7be4ce7cf41d4a
jeffreynicolas <jeffreynicolas.deus@gmail.com>
2024-07-12 16:50:16 -0300

    componentes

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ExpandBox from './components/ExpandBox.vue'
+</script>
+
+<template>
+  <h1>componentes</h1>
+  <ExpandBox />
+  <ExpandBox />
+  <expandbox/>
+</template>
+
+<style scoped></style>
---
+++ src/components/ExpandBox.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+import { ref} from 'vue';
+
+const showContent = ref(false)
+</script>
+
+<template>
+
+<div>
+<button @click="showContent=!showContent">mostrar</button>
+<div v-if="showContent" class="ExpandBox">
+Murilooooooooooooooooooooooooo
+</div>
+</div>
+
+</template>
+
+<style scoped>
+
+.ExpandBox{
+
+    height: 100%;
+    padding: 20px;
+    border: 1px solid black;
+    border-radius: 10px;
+    box-shadow: 3px 3px 2px 2px gray;
+}
+
+</style>
